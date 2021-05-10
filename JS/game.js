6e995c142dc94ec6c1ee8efca3a9abb848afe542
nick <gdfvsgfd8769@gmail.com>
2021-05-10 22:57:13 +0800

    git

--- JS/game.js
+++ JS/game.js
@@ -1,88 +1,84 @@
-let players = ['player1','player2'];
-let player = ["O","X"];
-let turn = 0;
-let continuous = false;
+// let players = ['player1','player2'];
+// let player = ["O","X"];
+// let turn = 0;
+// let continuous = false;
 
-let game = {
-    square: 2
-};
+// function lineHandler(){
+//     this.classList.add('selected');
+//     this.removeEventListener('click',lineHandler);
 
-function lineHandler(){
-    this.classList.add('selected');
-    this.removeEventListener('click',lineHandler);
+//     let row = parseInt(this.dataset.row);
+//     let num = parseInt(this.dataset.num);
+//     let section = this.parentElement.className;
+//     continuous = false;
 
-    let row = parseInt(this.dataset.row);
-    let num = parseInt(this.dataset.num);
-    let section = this.parentElement.className;
-    continuous = false;
+//     boxJudge(row,num,section);
 
-    boxJudge(row,num,section);
-
-    if(!continuous){
-        turn+=1;
-        turn === players.length ? turn = 0 : 0 ;
-    };
+//     if(!continuous){
+//         turn+=1;
+//         turn === players.length ? turn = 0 : 0 ;
+//     };
     
-};
+// };
 
-function boxJudge(row,num,section){
-    let condition1 = [false,false,false,"target"];
-    let condition2 = [false,false,false,"target"];
+// function boxJudge(row,num,section){
+//     let condition1 = [false,false,false,"target"];
+//     let condition2 = [false,false,false,"target"];
 
-    if(section === 'boxs'){
+//     if(section === 'boxs'){
 
-        if(num!==0){
+//         if(num!==0){
 
-            condition1 = [
-                document.querySelector(`[data-row="${row}"][data-num="${num-1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row-1}"][data-num="${num-1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row+1}"][data-num="${num-1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-box="${(row-1)/2}${num-1}"]`)
-            ];
+//             condition1 = [
+//                 document.querySelector(`[data-row="${row}"][data-num="${num-1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row-1}"][data-num="${num-1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row+1}"][data-num="${num-1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-box="${(row-1)/2}${num-1}"]`)
+//             ];
 
-        };
+//         };
 
-        if(num!==game.square){
+//         if(num!==game.square){
 
-            condition2 = [
-                document.querySelector(`[data-row="${row}"][data-num="${num+1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-box="${(row-1)/2}${num}"]`)
-            ];
+//             condition2 = [
+//                 document.querySelector(`[data-row="${row}"][data-num="${num+1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-box="${(row-1)/2}${num}"]`)
+//             ];
 
-        };
+//         };
 
-    }else{
+//     }else{
 
-        if(row!==0){
+//         if(row!==0){
 
-            condition1 = [
-                document.querySelector(`[data-row="${row-2}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row-1}"][data-num="${num+1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-box="${row/2-1}${num}"]`)
-            ];
+//             condition1 = [
+//                 document.querySelector(`[data-row="${row-2}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row-1}"][data-num="${num+1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-box="${row/2-1}${num}"]`)
+//             ];
 
-        };
+//         };
 
-        if(row!==game.square*2){
+//         if(row!==game.square*2){
 
-            condition2 = [
-                document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row+1}"][data-num="${num+1}"]`).classList.contains('selected'),
-                document.querySelector(`[data-row="${row+2}"][data-num="${num}"]`).classList.contains('selected'),
-                document.querySelector(`[data-box="${row/2}${num}"]`)
-            ];
+//             condition2 = [
+//                 document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row+1}"][data-num="${num+1}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-row="${row+2}"][data-num="${num}"]`).classList.contains('selected'),
+//                 document.querySelector(`[data-box="${row/2}${num}"]`)
+//             ];
 
-        };
+//         };
 
-    };
+//     };
 
-    boxWrite(condition1);
-    boxWrite(condition2);
+//     boxWrite(condition1);
+//     boxWrite(condition2);
 
-};
+// };
 
 function boxWrite(arr){
     let all = arr[0] && arr[1] && arr[2];
@@ -93,8 +89,8 @@ function boxWrite(arr){
     };
 };
 
-let lines = document.querySelectorAll('.line');
+// let lines = document.querySelectorAll('.line');
 
-lines.forEach( line => {
-    line.addEventListener('click',lineHandler);
-});
+// lines.forEach( line => {
+//     line.addEventListener('click',lineHandler);
+// });
--- JS/setting.js
+++ JS/setting.js
@@ -1,22 +1,36 @@
 let headerHeight = document.querySelector('header').getBoundingClientRect().height;
-let main = document.querySelector('.game .container .row');
+let main = document.querySelector('main');
 let gameset = document.querySelector('.gameset');
+
+let side = 0;
+let players = ['player1','player2'];
+let player = ["O","X"];
+let turn = 0;
+let continuous = false;
+
+let game = document.querySelector('.game .container .row');
 let renden = document.querySelectorAll('.gameset button');
 let html = '';
 
+let nameBtn = document.querySelectorAll('.player button');
+
 // document.querySelector('body').style.paddingTop = headerHeight + 'px';
 main.style.paddingTop = headerHeight + 'px';
-gameset.style.paddingTop = headerHeight + 'px';
+
+function changeName(){
+    console.log(this.dataset.status);
+}
 
 function rendenTable(){
-    let side = parseInt(this.value);
+    side = parseInt(this.value);
+    game.innerHTML = '';
     for(let i=0; i<=side ;i+=1){
-        main.innerHTML+=`
+        game.innerHTML+=`
             <div class="bar"></div>
             <div class="boxs"></div>
         `;
         if(i==side){
-            main.innerHTML+=`
+            game.innerHTML+=`
                 <div class="bar"></div>
             `;
         }
@@ -38,7 +52,7 @@ function rendenTable(){
             // console.log(item,index);
             item.innerHTML+= `
                 <div class="doc"></div>
-                <div class="line" data-row="${index}" data-num="${i}">
+                <div class="line" data-row="${index*2}" data-num="${i}">
                     <div></div>
                 </div>
             `;
@@ -55,7 +69,7 @@ function rendenTable(){
         for(let i=0; i<=side ;i+=1){
             // console.log(item,index);
             item.innerHTML+= `
-                <div class="line" data-row="${index}" data-num="${i}">
+                <div class="line" data-row="${1+index*2}" data-num="${i}">
                     <div></div>
                 </div>
                 <div class="box" data-box="${index}${i}">
@@ -72,8 +86,104 @@ function rendenTable(){
             }
         }
     });
+
+    let lines = document.querySelectorAll('.line');
+
+    lines.forEach( line => {
+        line.addEventListener('click',lineHandler);
+    });
 }
+
+//////////////////////////////////////////////////////
+function lineHandler(){
+    this.classList.add('selected');
+    this.removeEventListener('click',lineHandler);
+
+    let row = parseInt(this.dataset.row);
+    let num = parseInt(this.dataset.num);
+    let section = this.parentElement.className;
+    continuous = false;
+
+    boxJudge(row,num,section);
+
+    if(!continuous){
+        turn+=1;
+        turn === players.length ? turn = 0 : 0 ;
+    };
+    
+};
+
+function boxJudge(row,num,section){
+    let condition1 = [false,false,false,"target"];
+    let condition2 = [false,false,false,"target"];
+
+    if(section === 'boxs'){
+
+        if(num!==0){
+
+            console.log(row,num);
+            console.log(document.querySelector(`[data-row="${row}"][data-num="${num-1}"]`));
+            console.log(document.querySelector(`[data-row="${row-1}"][data-num="${num-1}"]`));
+            console.log(document.querySelector(`[data-row="${row+1}"][data-num="${num-1}"]`));
+
+            condition1 = [
+                document.querySelector(`[data-row="${row}"][data-num="${num-1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row-1}"][data-num="${num-1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row+1}"][data-num="${num-1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-box="${(row-1)/2}${num-1}"]`)
+            ];
+
+            
+        };
+
+        if(num!==side){
+
+            condition2 = [
+                document.querySelector(`[data-row="${row}"][data-num="${num+1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-box="${(row-1)/2}${num}"]`)
+            ];
+
+        };
+
+    }else{
+
+        if(row!==0){
+
+            condition1 = [
+                document.querySelector(`[data-row="${row-2}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row-1}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row-1}"][data-num="${num+1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-box="${row/2-1}${num}"]`)
+            ];
+
+        };
+
+        if(row!==side*2){
+
+            condition2 = [
+                document.querySelector(`[data-row="${row+1}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row+1}"][data-num="${num+1}"]`).classList.contains('selected'),
+                document.querySelector(`[data-row="${row+2}"][data-num="${num}"]`).classList.contains('selected'),
+                document.querySelector(`[data-box="${row/2}${num}"]`)
+            ];
+
+        };
+
+    };
+
+    boxWrite(condition1);
+    boxWrite(condition2);
+
+};
+
+//////////////////////////////////////////////////////
 
 renden.forEach( item => {
     item.addEventListener('click',rendenTable);
+});
+
+nameBtn.forEach( item => {
+    item.addEventListener('click',changeName);
 });
