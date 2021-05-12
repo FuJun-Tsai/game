ca006bbc1ed734b37f49d57766ce1c71c553d03b
nick <gdfvsgfd8769@gmail.com>
2021-05-12 22:21:37 +0800

    2105122230

--- JS/setting.js
+++ JS/setting.js
@@ -10,9 +10,10 @@ let players = {
     },
     player2:{
         playerName: 'player2',
-        playerColor: '##0000ff'
+        playerColor: '#0000ff'
     }
 };
+let data = JSON.parse(localStorage.getItem('game')) || players;
 
 let player = ["O","X"];
 let turn = 0;
@@ -27,12 +28,43 @@ let totalLine = 0;
 
 let nameBtn = document.querySelectorAll('.player button');
 let colorBtn = [...document.querySelectorAll('.player .layout .color')];
-// document.querySelector('body').style.paddingTop = headerHeight + 'px';
+
 main.style.paddingTop = headerHeight + 'px';
+
+function rendenWindow(){
+    console.log(data);
+    let name1 = document.querySelector('.player1 .name');
+    let name2 = document.querySelector('.player2 .name');
+    let color1 = document.querySelector('.player1 .color');
+    let color2 = document.querySelector('.player2 .color');
+    let score1 = document.querySelector('.player1 .layout .score');
+    let score2 = document.querySelector('.player2 .layout .score');
+
+
+    name1.textContent = data.player1.playerName;
+    name2.textContent = data.player2.playerName;
+    color1.value = data.player1.playerColor;
+    color2.value = data.player2.playerColor;
+    name1.style.color = data.player1.playerColor;
+    name2.style.color = data.player2.playerColor;
+    score1.style.color = data.player1.playerColor;
+    score2.style.color = data.player2.playerColor;
+
+    // player1.style.color = data.player1.playerColor;
+    // player2.style.color = data.player2.playerColor;
+    
+}
+
+rendenWindow();
+
 
 function changeName(){
     let playerName = this.parentElement.querySelector('.name');
     let inputName = this.parentElement.querySelector('.changename');
+    let dataChange = this.dataset.change;
+
+    console.log(dataChange);
+    console.log(data);
 
     if(this.dataset.status=='0'){
         playerName.style.display = 'none';
@@ -48,16 +80,23 @@ function changeName(){
         this.dataset.status = '0';
         this.innerText = '修改';
 
+        data[`player${dataChange}`].playerName = inputName.value;
+        localStorage.setItem('game',JSON.stringify(data));
     }
-
 }
 
 function changeColor(){
     let playerColor = this.parentElement.parentElement.querySelector('.name');
+    let playerScore = this.parentElement.querySelector('.score');
+    let dataChange = this.dataset.change;
+
     this.value == '#ffffff' ? playerColor.style.background = '#000' : playerColor.style.background = '' ;
     playerColor.style.color = this.value;
-    // console.log(colorBtn.indexOf(this)+1);
-    document.querySelector('html').style.setProperty(`--player${colorBtn.indexOf(this)+1}Color`,this.value);
+    playerScore.style.color = this.value;
+
+    data[`player${dataChange}`].playerColor = this.value;
+    localStorage.setItem('game',JSON.stringify(data));
+    
 }
 
 function rendenTable(){
